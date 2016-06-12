6457151ba6f0fb6316f0cea6c7a727969a6e195f
xiongchiamiov <xiong.chiamiov@gmail.com>
2016-06-11 19:17:15 -0700

    Refactor printy things into functions
    This is mostly to support upcoming UI work, so they don't get all
    tangled together.

--- ncfc.go
+++ ncfc.go
@@ -11,21 +11,8 @@ import (
 	"strings"
 )
 
-func main() {
-	// If this starts to become a performance problem when inspecting
-	// directories with large numbers of direct children, look into using
-	// os.File.Readdir instead to avoid the needless sort.
-	files, _ := ioutil.ReadDir(".")
-
-	directories := []os.FileInfo{}
-	nonDirectories := []os.FileInfo{}
-	for _, file := range files {
-		if file.IsDir() {
-			directories = append(directories, file)
-		} else {
-			nonDirectories = append(nonDirectories, file)
-		}
-	}
+func gather_directory_names(directories []os.FileInfo) []string {
+	names := []string{}
 
 	for _, directory := range directories {
 		find := exec.Command("find", directory.Name())
@@ -46,10 +33,45 @@ func main() {
 
 		total, _ := strconv.Atoi(strings.TrimSpace(buffer.String()))
 
-		fmt.Printf("%s\t%d\n", directory.Name(), total)
+		names = append(names, fmt.Sprintf("%s\t%d", directory.Name(), total))
+	}
+
+	return names
+}
+
+func gather_non_directory_names(nonDirectories []os.FileInfo) []string {
+	names := []string{}
+
+	for _, nonDirectory := range nonDirectories {
+		names = append(names, nonDirectory.Name())
+	}
+
+	return names
+}
+
+func main() {
+	// If this starts to become a performance problem when inspecting
+	// directories with large numbers of direct children, look into using
+	// os.File.Readdir instead to avoid the needless sort.
+	files, _ := ioutil.ReadDir(".")
+
+	directories := []os.FileInfo{}
+	nonDirectories := []os.FileInfo{}
+	for _, file := range files {
+		if file.IsDir() {
+			directories = append(directories, file)
+		} else {
+			nonDirectories = append(nonDirectories, file)
+		}
+	}
+
+	directoryNames := gather_directory_names(directories)
+	for _, directoryName := range directoryNames {
+		fmt.Println(directoryName)
 	}
 	fmt.Println(strings.Repeat("-", 80))
-	for _, nonDirectory := range nonDirectories {
-		fmt.Println(nonDirectory.Name())
+	nonDirectoryNames := gather_non_directory_names(nonDirectories)
+	for _, nonDirectoryName := range nonDirectoryNames {
+		fmt.Println(nonDirectoryName)
 	}
 }
